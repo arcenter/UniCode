f988aa1060236bbb198b2989d7e0cbcc01ee9254
ArCenter <101260108+arcenter@users.noreply.github.com>
2023-10-10 18:51:13 +0400

    Corrections
    Changed binary direction from L→R to R→L

--- DD Lab Assignments/E5A1.sv
+++ DD Lab Assignments/E5A1.sv
@@ -26,14 +26,14 @@ endmodule
 
 module e5a1(output reg [2:0]o, input [7:0]i);
   always @ (*) begin
-    if (i[7] == 1) o = 3'b000;
-    else if (i[6] == 1) o = 3'b001;
-    else if (i[5] == 1) o = 3'b010;
-    else if (i[4] == 1) o = 3'b011;
-    else if (i[3] == 1) o = 3'b100;
-    else if (i[2] == 1) o = 3'b101;
-    else if (i[1] == 1) o = 3'b110;
-    else if (i[0] == 1) o = 3'b111;
+    if (i[0] == 1) o = 3'b000;
+    else if (i[1] == 1) o = 3'b001;
+    else if (i[2] == 1) o = 3'b010;
+    else if (i[3] == 1) o = 3'b011;
+    else if (i[4] == 1) o = 3'b100;
+    else if (i[5] == 1) o = 3'b101;
+    else if (i[6] == 1) o = 3'b110;
+    else if (i[7] == 1) o = 3'b111;
     else o = 3'bxxx;
   end
 endmodule
